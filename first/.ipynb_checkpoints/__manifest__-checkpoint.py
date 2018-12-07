20a5abcdf9736353de8ac563b670787f35f3032c
li <20901678@qq.com>
2018-12-07 11:02:07 +0000

    day11

---
+++ first/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'PS Cloud 培训',
+    'version': '12.0.1.0',
+    'summary': 'PS Cloud 培训',
+    'author': "www.mypscloud.com",
+    'website': 'https://www.mypscloud.com/',
+    'category': 'Tools',
+    'depends': ['base'],
+    'qweb': [],
+    'js': [],
+    'css': [],
+    'auto_install': False,
+    'application': True,
+}
